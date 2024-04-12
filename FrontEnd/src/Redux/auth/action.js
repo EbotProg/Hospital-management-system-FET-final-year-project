b798d2cbd46ce30c794c6f74e18f13199b3eb9ac
Achale Ebot <achaleebotoma2002@gmail.com>
2024-04-12 18:06:47 +0100

    initial commit

---
+++ FrontEnd/src/Redux/auth/action.js
@@ -0,0 +1,277 @@
+import * as types from "./types";
+import axios from "axios";
+import { API } from "../../config";
+
+//login user
+export const NurseLogin = (data) => async (dispatch) => {
+  try {
+    console.log('api', API)
+    dispatch({ type: types.LOGIN_NURSE_REQUEST });
+    const res = await axios.post(
+      `${API}/nurses/login`,
+      data
+    );
+    dispatch({
+      type: types.LOGIN_NURSE_SUCCESS,
+      payload: {
+        message: res.data.message,
+        user: res.data.user,
+        token: res.data.token,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    dispatch({
+      type: types.LOGIN_NURSE_ERROR,
+      payload: {
+        message: error,
+      },
+    });
+  }
+};
+
+//login user
+export const DoctorLogin = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: types.LOGIN_DOCTOR_REQUEST });
+    const res = await axios.post(
+      `${API}/doctors/login`,
+      data
+    );
+    console.log(res.data);
+    dispatch({
+      type: types.LOGIN_DOCTOR_SUCCESS,
+      payload: {
+        message: res.data.message,
+        user: res.data.user,
+        token: res.data.token,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    dispatch({
+      type: types.LOGIN_DOCTOR_ERROR,
+      payload: {
+        message: error,
+      },
+    });
+  }
+};
+
+//login user
+export const AdminLogin = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: types.LOGIN_ADMIN_REQUEST });
+    const res = await axios.post(
+      `${API}/admin/login`,
+      data
+    );
+    console.log(res.data);
+    dispatch({
+      type: types.LOGIN_ADMIN_SUCCESS,
+      payload: {
+        message: res.data.message,
+        user: res.data.user,
+        token: res.data.token,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    dispatch({
+      type: types.LOGIN_ADMIN_ERROR,
+      payload: {
+        message: error,
+      },
+    });
+  }
+};
+
+// REGISTER DOCTOR
+export const DoctorRegister = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: types.REGISTER_DOCTOR_REQUEST });
+    const res = await axios.post(
+      `${API}/doctors/register`,
+      data
+    );
+    // console.log(res);
+    return res.data;
+    // dispatch({
+    //   type: types.REGISTER_DOCTOR_SUCCESS,
+    //   payload: {
+    //     message: res.data.message,
+    //     user: res.data.user,
+    //     // token: res.data.token,
+    //     report: res.data.report,
+    //   },
+    // });
+  } catch (error) {
+    dispatch({
+      type: types.REGISTER_DOCTOR_ERROR,
+      payload: {
+        message: error,
+      },
+    });
+  }
+};
+
+// REGISTER NURSE
+export const NurseRegister = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: types.REGISTER_NURSE_REQUEST });
+    // const res = await axios.post(
+    //   "https://zany-gray-clam-gear.cyclic.app/nurses/register",
+    //   data
+    // );
+    const res = await axios.post(
+      `${API}/nurses/login`,
+      data
+    );
+    // console.log(res);
+    return res.data;
+    // dispatch({
+    //   type: types.REGISTER_NURSE_SUCCESS,
+    //   payload: {
+    //     message: res.data.message,
+    //     user: res.data.user,
+    //     // token: res.data.token,
+    //     report: res.data.report,
+    //   },
+    // });
+  } catch (error) {
+    dispatch({
+      type: types.REGISTER_NURSE_ERROR,
+      payload: {
+        message: error,
+      },
+    });
+  }
+};
+
+// REGISTER ADMIN
+export const AdminRegister = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: types.REGISTER_ADMIN_REQUEST });
+    const res = await axios.post(
+      `${API}/admin/register`,
+      data
+    );
+    // console.log(res);
+    return res.data;
+    // dispatch({
+    //   type: types.REGISTER_ADMIN_SUCCESS,
+    //   payload: {
+    //     message: res.data.message,
+    //     user: res.data.user,
+    //     // token: res.data.token,
+    //     report: res.data.report,
+    //   },
+    // });
+  } catch (error) {
+    dispatch({
+      type: types.REGISTER_ADMIN_ERROR,
+      payload: {
+        message: error,
+      },
+    });
+  }
+};
+
+// REGISTER AMBULANCE
+export const AmbulanceRegister = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: types.REGISTER_AMBULANCE_REQUEST });
+    const res = await axios.post(
+      `${API}/ambulances/add`,
+      data
+    );
+    console.log(res);
+    // dispatch({
+    //   type: types.REGISTER_AMBULANCE_SUCCESS,
+    //   payload: {
+    //     message: res.data.message,
+    //     user: res.data.user,
+    //     // token: res.data.token,
+    //     report: res.data.report,
+    //   },
+    // });
+  } catch (error) {
+    dispatch({
+      type: types.REGISTER_AMBULANCE_ERROR,
+      payload: {
+        message: error,
+      },
+    });
+  }
+};
+
+// logout user
+export const authLogout = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: types.AUTH_LOGOUT,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//update nurse
+export const UpdateNurse = (data, id) => async (dispatch) => {
+  try {
+    dispatch({ type: types.EDIT_NURSE_REQUEST });
+    const res = await axios.patch(
+      `${API}/nurses/${id}`,
+      data
+    );
+    console.log(res);
+    dispatch({ type: types.EDIT_NURSE_SUCCESS, payload: res.data.user });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//update doctor
+export const UpdateDoctor = (data, id) => async (dispatch) => {
+  try {
+    dispatch({ type: types.EDIT_DOCTOR_REQUEST });
+    const res = await axios.patch(
+      `${API}/doctors/${id}`,
+      data
+    );
+    console.log(res);
+    dispatch({ type: types.EDIT_DOCTOR_SUCCESS, payload: res.data.user });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//update doctor
+export const SendPassword = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: types.EDIT_DOCTOR_REQUEST });
+    const res = await axios.post(
+      `${API}/admin/password`,
+      data
+    );
+    // console.log(res);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//update doctor
+export const forgetPassword = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: types.FORGET_PASSWORD_REQUEST });
+    const res = await axios.post(
+      `${API}/admin/forgot`,
+      data
+    );
+    // console.log(res);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
